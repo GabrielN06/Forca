7e6d8b8d1b87004c13f5c4445e40d7a55f998b64
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-03-23 13:01:37 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Forca
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("--- Forca ---");
+
+        Console.Write("Qual a palavra secreta? ");
+        string? palavraSecreta = Console.ReadLine(); 
+
+        Console.Write("Qual a letra? ");
+        char letra = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+       string resultado = (palavraSecreta ?? "").Contains(letra) 
+            ? $"A letra \"{letra}\" existe na palavra secreta => True" 
+            : $"A letra \"{letra}\" não existe na palavra secreta => False"; 
+
+        Console.WriteLine(resultado);
+    }
+}
+
+
+
